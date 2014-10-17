d5f53a1e9f7e9579be3fe063edb84950243e022b
j.taneichi <j.tane3ma@gmail.com>
2014-10-17 12:54:21 +0900

    edit js

--- js/scrollSmoke.coffee
+++ js/scrollSmoke.coffee
@@ -2,24 +2,35 @@ class window.scrollSmoke
   constructor: (options) ->
     @$scrollWatch = options.$scrollWatch
     @$positionWatch = options.$positionWatch
-    @$windowHeight = $(window).height()
     @$smoke        = options.$smoke
     @$header       = options.$header
     @npos = 0
-    @$scrollWatch.on "scroll", =>
+    @showWord = false
+
+    @$scrollWatch.on "resize scroll", =>
+      @$windowHeight = $(window).height()
       @npos = @$positionWatch.position().top
       if -@npos < @$windowHeight
-        @opacity =  -@npos / (@$windowHeight - @$header.outerHeight())
-        @$smoke.css
-          "opacity" : @opacity
-        if @opacity > 1
-          @$header.css
-            "opacity": 1
-            "background-color" : "rgb( 63, 104, 162)"
+        @smokeAnimation()
+        if @opacity > 0.98
+          @$header.show()
+          @$header.find(":first-child").stop().animate
+            opacity: 1
+            "easing": "swing"
+            100
         else
-          @$header.css
-            "opacity" : 0
+          @$header.hide()
+          @$header.find(":first-child").css
+            opacity: 0
       else
-        @$header.css
-          opacity: 1
-          "background-color" : "rgb( 63, 104, 162)"
+        @$smoke.css
+          "opacity" : 1
+
+
+
+  smokeAnimation: ->
+    @opacity =  -@npos / (@$windowHeight - @$header.outerHeight())
+    @$smoke.css
+      "opacity" : @opacity
+    @$scrollWatch.css
+      "background-position-y" : -@opacity * 100
--- js/scrollSmoke.js
+++ js/scrollSmoke.js
@@ -3,37 +3,46 @@
     function scrollSmoke(options) {
       this.$scrollWatch = options.$scrollWatch;
       this.$positionWatch = options.$positionWatch;
-      this.$windowHeight = $(window).height();
       this.$smoke = options.$smoke;
       this.$header = options.$header;
       this.npos = 0;
-      this.$scrollWatch.on("scroll", (function(_this) {
+      this.showWord = false;
+      this.$scrollWatch.on("resize scroll", (function(_this) {
         return function() {
+          _this.$windowHeight = $(window).height();
           _this.npos = _this.$positionWatch.position().top;
           if (-_this.npos < _this.$windowHeight) {
-            _this.opacity = -_this.npos / (_this.$windowHeight - _this.$header.outerHeight());
-            _this.$smoke.css({
-              "opacity": _this.opacity
-            });
-            if (_this.opacity > 1) {
-              return _this.$header.css({
-                "opacity": 1,
-                "background-color": "rgb( 63, 104, 162)"
-              });
+            _this.smokeAnimation();
+            if (_this.opacity > 0.98) {
+              _this.$header.show();
+              return _this.$header.find(":first-child").stop().animate({
+                opacity: 1,
+                "easing": "swing"
+              }, 100);
             } else {
-              return _this.$header.css({
-                "opacity": 0
+              _this.$header.hide();
+              return _this.$header.find(":first-child").css({
+                opacity: 0
               });
             }
           } else {
-            return _this.$header.css({
-              opacity: 1,
-              "background-color": "rgb( 63, 104, 162)"
+            return _this.$smoke.css({
+              "opacity": 1
             });
           }
         };
       })(this));
     }
+
+    scrollSmoke.prototype.smokeAnimation = function() {
+      this.opacity = -this.npos / (this.$windowHeight - this.$header.outerHeight());
+      this.$smoke.css({
+        "opacity": this.opacity
+      });
+      return this.$scrollWatch.css({
+        "background-position-y": -this.opacity * 100
+      });
+    };
 
     return scrollSmoke;
 
